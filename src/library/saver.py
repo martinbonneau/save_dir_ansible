8654c35b308930f71c4c45698054a1259ff24109
Martin Bonneau <martin.bonneau@etudiant.univ-lr.fr>
2020-04-29 15:04:49 +0200

    Check if path is file or directory
    - add imports
    - add simple path verification
    - adjust module documentation
    - adjuste example playbook

--- src/library/saver.py
+++ src/library/saver.py
@@ -2,22 +2,27 @@
 # -*- coding: utf-8 -*-
 
 from ansible.module_utils.basic import AnsibleModule
+from os import walk as Walk
+from os.path import exists as Exists
+from os.path import isfile as Isfile
+from os.path import isdir as Isdir
+
 
 DOCUMENTATION = """
-module: module_name
-author: author_name
-description : description
+module: saver
+author: Martin Bonneau
+description : Allow to save a file or a folder to another storage server. Can do full or incremental save.
 
 options:
-    arg:
-        description: description of arg
-        required : yes/no
+    path_to_save:
+        description: The file or folder path to save
+        required : yes
 """
 
 EXAMPLES = """
-- name : "Task name example"
-  module_name:
-    arg: "value"
+- name : "Example task name"
+  saver:
+    path_to_save: "/var/www/"
 """
 
 RETURN = """
@@ -30,15 +35,29 @@ def main():
     module = AnsibleModule(
         argument_spec = dict(
             #arguments here
-            arg = dict(required=True, type='str'),
+            path_to_save = dict(required=True, type='str'),
         )
     )
 
     #get params
-    arg = module.params.get("arg")
+    path_to_save = module.params.get("path_to_save")
 
-    #do something
-    output = arg
+    #check if the path exists and if it's a file or a directory
+    if(Exists(path_to_save)):
+
+        if (Isfile(path_to_save)):
+            #path is a single file
+            output = "single file"
+
+        elif (Isdir(path_to_save)):
+            #path is a directory
+            output = "directory"
+
+        else:
+            output = "The given path is not a file or a directory."
+
+    else:
+        output = "The given path doesn't exist on the host."
 
     #export something to ansible output
     module.exit_json(changed=True, ansible_module_results=output)
